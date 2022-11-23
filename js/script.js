6d5faf1185be45c5ccfd348ec591b28ba30cc168
Piersilvio_Pellegrini <112377026+PiersilvioPellegrini@users.noreply.github.com>
2022-11-23 10:15:17 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,70 @@
+const { createApp } = Vue;
+
+const app = createApp({
+  data() {
+    return {
+      myInsterval:null,
+      currentIndex: 0,
+      movie: [
+        {
+          image: "img/01.webp",
+          title: "Marvel's Spiderman Miles Morale",
+          text: "Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.",
+        },
+        {
+          image: "img/02.webp",
+          title: "Ratchet & Clank: Rift Apart",
+          text: "Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.",
+        },
+        {
+          image: "img/03.webp",
+          title: "Fortnite",
+          text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+        },
+        {
+          image: "img/04.webp",
+          title: "Stray",
+          text: "Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city",
+        },
+        {
+          image: "img/05.webp",
+          title: "Marvel's Avengers",
+          text: "Marvel's Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.",
+        },
+      ]
+    }
+    },
+  methods: {
+    next() {
+      if (this.currentIndex === this.movie.length - 1) {
+        this.currentIndex = 0;
+      } else {
+        this.currentIndex++;
+      }
+    },
+
+    prev() {
+      if (this.currentIndex === 0) {
+        this.currentIndex = this.movie.length - 1;
+      } else {
+        this.currentIndex--;
+      }
+    },
+
+    onclickImg(index) {
+      this.currentIndex = index;
+    },
+
+    autoPlay() {
+       myInterval = setInterval(this.next, 2000);
+    },
+
+    stopAutoPlay() {
+      clearInterval(myInterval);
+    },
+  },
+    mounted() {
+        this.autoPlay();
+  },
+
+}).mount("#app");
